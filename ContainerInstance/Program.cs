7721f5a398d1c64e52b58a70aa140a57ab91df67
Tsuyoshi Ushio <ushio@simplearchitect.com>
2018-01-11 07:31:35 +0900

    Edit for actual stress testing

--- ContainerInstance/Program.cs
+++ ContainerInstance/Program.cs
@@ -22,38 +22,46 @@ namespace ContainerInstance
             var client = new ContainerInstanceManagementClient(credentials);
             client.SubscriptionId = subsctiptionId;
 
+
             // Creating 1000 containers
             // We can only 20 instances at the same time. Then we need to wait for 1.5 min. 
             // Microsoft.Rest.Azure.CloudException: 'Number of requests for action 'ContainerGroupCreation' exceeded the limit of '20' for time interval '00:05:00'. Please try again after '1.25' minutes.'
             // Microsoft.Rest.Azure.CloudException: 'Resource type 'Microsoft.ContainerInstance/containerGroups' container group quota '20' exceeded in region 'East US'.'
             // I send a support request for change the quota. https://docs.microsoft.com/en-us/azure/container-instances/container-instances-quotas
-            var loop = Enumerable.Range(0, 3);
+            var loop = Enumerable.Range(0, 2); // upto 3
             var locations = new string[] { "EastUS", "WestUS", "WestEurope" }; // Currently available only three resion. 
-            var innerLoop = Enumerable.Range(0, 20);
 
-            foreach (var y in loop)
+
+            var containerGroupNumberRange = Enumerable.Range(0, 10); // upto 20
+
+
+            //  Create clients
+            foreach (var i in containerGroupNumberRange)
             {
-                
-                Parallel.ForEach(innerLoop, async x =>
-                {
-                    await Create10Instances(client, $"containerGroup{y}-{x}", locations[y]);
-                });
-
-                await Task.Delay(TimeSpan.FromMinutes(5));
+                await Create10Instances(client, $"containerGroup0-{i}", "EastUS", 0, $"container-{1}");
             }
+
+            // wait for the all Client has been created. 
+            await Task.Delay(TimeSpan.FromMinutes(1));
+
+
+            foreach (var i in containerGroupNumberRange) { 
+                await Create10Instances(client, $"containerGroup1-{i}", "EastUS", 1, $"container-{i}");
+            }
+
             Console.WriteLine("Done. Press button");
             Console.ReadLine();
 
             // 1000 concurrent request now for 1 min
-            await Task.Delay(TimeSpan.FromMinutes(1));
+           // await Task.Delay(TimeSpan.FromMinutes(1));
             // Delete 1000 containers
             foreach (var y in loop)
             {
-                Parallel.ForEach(innerLoop, async x =>
+                foreach(var x in containerGroupNumberRange)
                 {
                     await DeleteInstance(client, $"containerGroup{y}-{x}");
-                });
-                await Task.Delay(TimeSpan.FromMinutes(2));
+                }
+
             }
 
             Console.WriteLine("Done. Deleted");
@@ -65,30 +73,68 @@ namespace ContainerInstance
             await client.ContainerGroups.DeleteAsync(resourceGroup, containerGroup);
         }
 
-        private static async Task Create10Instances(ContainerInstanceManagementClient client, string containerGroup, string location)
+        private static async Task Create10Instances(ContainerInstanceManagementClient client, string containerGroup, string location, int flag, string guid)
         {
             var resources = new ResourceRequirements();
             var request = new ResourceRequests();
-            request.Cpu = 0.1;
-            request.MemoryInGB = 0.1;
+            request.Cpu = 0.2;
+            request.MemoryInGB = 0.3; // once I tried 0.1 it cause a long execution time. 
 
             resources.Requests = request;
-            var container = new Container("nginx", "nginx", resources);
+            
             var list = new List<Container>();
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < 5; i++)
             {
-                list.Add(new Container($"nginx{i}", "nginx", resources));
+                if (flag == 0)
+                {
+                    list.Add(CreateClientContainer("efitnesstest/client", i, resources, $"{guid}-{i}"));
+                }
+                else
+                {
+                    list.Add(CreateSpamerContainer("efitnesstest/spamer", i, resources, $"{guid}-{i}"));
+                }
             }
+
+
             await client.ContainerGroups.CreateOrUpdateAsync(resourceGroup, containerGroup,
                 new ContainerGroup()
                 {
                     Containers = list,
                     Location = location,
-                    OsType = "Linux"
-
+                    OsType = "Linux",
+                    RestartPolicy = "Never"   // try not to emit the message by restarting
                 }
                 );
             Console.WriteLine($"Done for {containerGroup}");
+        }
+
+        private static  Container CreateSpamerContainer(string name, int i, ResourceRequirements resources, string guid)
+        {
+            Console.WriteLine($"Create Container: spamer{i}, {name}, guid: {guid}, queName: que1, messageCount: 10 Connection:{ConfigurationManager.AppSettings.Get("StorageConnectionString")}");
+            var container = new Container($"spamer{i}", name, resources);
+            var list = new List<EnvironmentVariable>();
+            list.Add(new EnvironmentVariable("StorageConnectionString", ConfigurationManager.AppSettings.Get("StorageConnectionString")));
+            list.Add(new EnvironmentVariable("guid", guid));
+            list.Add(new EnvironmentVariable("queName", "que1"));
+            list.Add(new EnvironmentVariable("messagesCount", "2")); // original 10 
+
+            container.EnvironmentVariables = list;
+            return container;
+
+        }
+
+        private static Container CreateClientContainer(string name, int i, ResourceRequirements resources, string guid)
+        {
+            Console.WriteLine($"Create Container: client{i}, {name}, DeviceID: {guid}, Connection:{ConfigurationManager.AppSettings.Get("StorageConnectionString")}");
+
+            var container = new Container($"client{i}", name, resources);
+            var list = new List<EnvironmentVariable>();
+            list.Add(new EnvironmentVariable("StorageConnectionString", ConfigurationManager.AppSettings.Get("StorageConnectionString")));
+            list.Add(new EnvironmentVariable("DeviceID", guid));
+
+            container.EnvironmentVariables = list;
+            return container;
+
         }
 
         private static string resourceGroup = ConfigurationManager.AppSettings.Get("resourceGroup");
